f833ff2dd658817120bcf8f9e7c859357e53bd07
yinqi <1204623070@qq.com>
2018-09-02 10:06:10 +0800

    Initial commit

---
+++ sql/examination_system.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : guting
+Source Server Version : 50553
+Source Host           : localhost:3306
+Source Database       : examination_system
+
+Target Server Type    : MYSQL
+Target Server Version : 50553
+File Encoding         : 65001
+
+Date: 2018-03-12 00:22:46
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
